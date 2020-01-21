ed592bf09672e88ea32ca5ab93b0e2a62ea74bf0
BolshakovDmitry <57066424+BolshakovDmitry@users.noreply.github.com>
2020-01-21 07:03:37 +0300

    Add files via upload

---
+++ spring-crm-rest-demo/src/main/java/org/bolshakov/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package org.bolshakov.springdemo.dao;
+
+import java.util.List;
+
+import org.bolshakov.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
